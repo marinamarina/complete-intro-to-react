0edd54b14c1c6aaab428d0efbb984447f9e09a17
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-21 19:50:12 +0100

    Composite components.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,18 @@
 var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
-var myComponent = div({style:{background: 'blue', height: '100px'}}, h1({style: {color: 'yellow'}}, 'Hello React'));
+var MyTitle = React.createClass({
+  render: function() {
+    return h1({ style: {color: 'yellow'} }, 'Hello React!');
+  }
+});
+
+var myComponent = div(
+  {style:
+    {background: 'blue', height: '100px'}
+  },
+  React.createElement(MyTitle, null),
+  React.createElement(MyTitle, null)
+);
 
 ReactDOM.render(myComponent, document.getElementById('app'));
