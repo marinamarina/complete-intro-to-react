001d47e634179086873f051e032d271021b97b58
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-22 23:21:36 +0100

    Use semistandard over standard.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,19 @@
 /* global React ReactDOM */
-var div = React.DOM.div
-var h1 = React.DOM.h1
+var div = React.DOM.div;
+var h1 = React.DOM.h1;
 
 var MyTitle = React.createClass({
   render: function () {
     return (
        h1({style: {color: this.props.color}}, this.props.title)
-    )
+    );
   }
-})
+});
 
 var myComponent = div(
   null,
   React.createElement(MyTitle, {color: 'peru', title: 'Hello React'}),
   React.createElement(MyTitle, {color: 'rebeccapurple', title: 'Hello cat'})
-)
+);
 
-ReactDOM.render(myComponent, document.getElementById('app'))
+ReactDOM.render(myComponent, document.getElementById('app'));
