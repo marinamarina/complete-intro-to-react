1ef537d6d09ddbf88acf5fcde6965d2006b8b157
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-22 23:19:54 +0100

    Use standard.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,19 @@
-var div = React.DOM.div;
-var h1 = React.DOM.h1;
+/* global React ReactDOM */
+var div = React.DOM.div
+var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-  render: function() {
+  render: function () {
     return (
-       h1(null, this.props.title)
-    );
+       h1({style: {color: this.props.color}}, this.props.title)
+    )
   }
-});
+})
 
 var myComponent = div(
   null,
-  React.createElement(MyTitle, {title: 'Hello React'}),
-  React.createElement(MyTitle, {title: 'Hello cat'})
-);
+  React.createElement(MyTitle, {color: 'peru', title: 'Hello React'}),
+  React.createElement(MyTitle, {color: 'rebeccapurple', title: 'Hello cat'})
+)
 
-ReactDOM.render(myComponent, document.getElementById('app'));
+ReactDOM.render(myComponent, document.getElementById('app'))
