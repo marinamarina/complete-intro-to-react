76538fba3c796ba80a327978f6eea11a764947fe
Marina Shchukina <shchukina.marina@gmail.com>
2016-09-24 09:04:43 +0100

    Using webpack.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,19 +1,11 @@
 /* global React ReactDOM */
 var div = React.DOM.div;
-var h1 = React.DOM.h1;
-
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-       h1({style: {color: this.props.color}}, this.props.title)
-    );
-  }
-});
+var MyTitle = require('./MyTitle');
 
 var myComponent = div(
   null,
   React.createElement(MyTitle, {color: 'peru', title: 'Hello React'}),
-  React.createElement(MyTitle, {color: 'rebeccapurple', title: 'Hello cat'})
+  React.createElement(MyTitle, {color: 'rebeccapurple', title: 'Hellooo cat'})
 );
 
 ReactDOM.render(myComponent, document.getElementById('app'));
