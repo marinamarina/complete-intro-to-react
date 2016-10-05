1c12cb951538858d0fc71fd0a65bc7013afa98ac
Marina Shchukina <shchukina.marina@gmail.com>
2016-10-05 17:47:20 +0100

    Minor changes

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,14 +2,12 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var MyTitle = require('./MyTitle.jsx');
 
-var myComponent = function() {
-  return (
+var MyComponent = () => (
     <div>
       <MyTitle color='peru' title='Hello Peru' />
       <MyTitle color='rebeccapurple' title='Hello cats' />
       <MyTitle color='#fo6do6' title='omg' />
     </div>
   )
-};
 
-ReactDOM.render(myComponent, document.getElementById('app'));
+ReactDOM.render(<MyComponent />, document.getElementById('app'));
