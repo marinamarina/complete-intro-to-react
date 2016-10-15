84f138e06365752d04cfb6a49f2f5de2c0976033
Marina Shchukina <shchukina.marina@gmail.com>
2016-10-15 16:33:38 +0100

    Added search input.

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,12 +3,20 @@ const data = require('../public/data')
 const ShowCard = require('./ShowCard.jsx')
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'this is my search term'
+    }
+  },
+  handleSearchEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='container'>
         <header className='header'>
-          <h1 className='brand'>svideo</h1>
-          <input className='search-input' type='text' placeholder='Search' />
+          <h1 className='brand'>{this.state.searchTerm}</h1>
+          <input className='search-input' type='text' placeholder={this.state.searchTerm} onChange={this.handleSearchEvent} />
         </header>
         <div className='shows'>
           {data.shows.map(show => (
