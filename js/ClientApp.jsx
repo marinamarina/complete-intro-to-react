08b06b21d430db82ec1868f800f774e0b503cde8
Marina Shchukina <shchukina.marina@gmail.com>
2016-10-15 10:02:16 +0100

    Pulling Layout wrapper into its own component.

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,13 +2,16 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing.jsx')
 const Search = require('./Search.jsx')
+const Layout = require('./Layout.jsx')
 
-const { Router, Route, hashHistory } = require('react-router')
+const { Router, Route, hashHistory, IndexRoute } = require('react-router')
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout} >
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 
