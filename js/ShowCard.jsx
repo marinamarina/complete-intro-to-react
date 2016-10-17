70fc1cf818780c4feb72eaa0ded8dfb3ddd29146
Marina Shchukina <shchukina.marina@gmail.com>
2016-10-17 14:00:06 +0100

    Linking from the seacrh results to the details page.

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,10 +1,11 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 const ShowCard = (props) => (
   <div className='show-card'>
     <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
     <div className='show-card-text'>
-      <h3 className='show-card-title'>{props.title}</h3>
+      <Link className='show-card-title' to={`/show/${props.imdbID}`}>{props.title} </Link>
       <h4 className='show-card-year'>({props.year}) </h4>
       <p className='show-card-description'>{props.description}</p>
     </div>
@@ -14,6 +15,7 @@ const ShowCard = (props) => (
 const { string } = React.PropTypes
 
 ShowCard.propTypes = {
+  imdbID: string.isRequired,
   poster: string.isRequired,
   title: string.isRequired,
   year: string.isRequired,
