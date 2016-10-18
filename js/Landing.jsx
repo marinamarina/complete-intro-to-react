544838a1bba3dccf438a3a29ff54fd5bff3cae7e
Marina Shchukina <shchukina.marina@gmail.com>
2016-10-18 17:36:12 +0100

    Implement search from the landing page.

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,33 @@
 const React = require('react')
-const {Link} = require('react-router')
+const { Link } = require('react-router')
+const { func, string } = React.PropTypes
+const { hashHistory } = require('react-router')
+const { connector } = require('./Store.jsx')
 
-const Landing = () => (
-  <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <Link to='/search' className='browse-all'> or Browse All</Link>
-  </div>
-)
+const Landing = React.createClass({
+  propTypes: {
+    setSearchTerm: func,
+    goToSearch: func,
+    searchTerm: string
+  },
+  goToSearch (e) {
+    e.preventDefault()
+    hashHistory.push('search')
+  },
+  handleSearchEvent (e) {
+    this.props.setSearchTerm(e.target.value)
+  },
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className='title'>svideo</h1>
+        <form onSubmit={this.goToSearch}>
+          <input className='search' type='text' placeholder='Search' onChange={this.handleSearchEvent} value={this.props.searchTerm} />
+          <Link to='/search' className='browse-all'> or Browse All</Link>
+        </form>
+      </div>
+    )
+  }
+})
 
-module.exports = Landing
+module.exports = connector(Landing)
