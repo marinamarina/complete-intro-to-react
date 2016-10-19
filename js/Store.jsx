0d101ba64e42e7d49331395330368ecd487d5fb6
Marina Shchukina <shchukina.marina@gmail.com>
2016-10-19 22:16:51 +0100

    Fixed a bug when store was not initialised.

--- js/Store.jsx
+++ js/Store.jsx
@@ -1,4 +1,4 @@
-const {redux, compose} = require('redux')
+const {compose, createStore} = require('redux')
 const reactRedux = require('react-redux')
 const { assign } = require('lodash')
 
@@ -43,7 +43,7 @@ const searchTermReducer = (state, action) => {
   assign(newState, state, {searchTerm: action.value})
   return newState
 }
-const store = redux.createStore(rootReducer, initialState, compose(
+const store = createStore(rootReducer, initialState, compose(
       window.devToolsExtension ? window.devToolsExtension() : f => f
     ))
 const connector = reactRedux.connect(mapStateToProps, mapDispatchToProps)
